2248856e1b9b2c96403d3143db925935a1cd16d8
KoleBarnes <kbarnes261@gmail.com>
2021-03-05 09:42:16 -0800

    WIP - Added CSV format option for metrics
    Signed-off-by: KoleBarnes <kbarnes261@gmail.com>

--- fetch-ledger-tx/plugins/metrics/sovrin_network_metrics.py
+++ fetch-ledger-tx/plugins/metrics/sovrin_network_metrics.py
@@ -3,12 +3,13 @@ import json
 import os
 import datetime
 import time
+import csv
 from .google_sheets import gspread_authZ
 from fetch_ledger_tx import get_txn_range
 
 class main(plugin_collection.Plugin):
     
-    def __init__(self, mlog: bool = False, gauth_json: str = None, file_name: str = None, worksheet_name: str = None, batchsize: int = None):
+    def __init__(self, mlog: bool = False, csv: bool = False, gauth_json: str = None, file_name: str = None, worksheet_name: str = None, batchsize: int = None):
         # metrics_log_info: list = []
         super().__init__()
         self.index = 1
@@ -16,6 +17,7 @@ class main(plugin_collection.Plugin):
         self.description = ''
         self.type = ''
         self.mlog = mlog
+        self.csv = csv
         self.gauth_json = gauth_json
         self.file_name = file_name
         self.worksheet_name = worksheet_name
@@ -23,10 +25,11 @@ class main(plugin_collection.Plugin):
 
     def parse_args(self, parser, argv=None, status_only: bool = False):
         parser.add_argument("--mlog", action="store_true", help="Metrics log argument uses google sheets api and requires, Google API Credentials json file name (file must be in root folder), google sheet file name and worksheet name. ex: --mlog --batchsize [Number (Not Required)] --json [Json File Name] --file [Google Sheet File Name] --worksheet [Worksheet name]")
-        parser.add_argument("--json", default=os.environ.get('JSON') , help="Google API Credentials json file name (file must be in root folder). Can be specified using the 'JSON' environment variable.", nargs='*')
-        parser.add_argument("--file", default=os.environ.get('FILE') , help="Specify which google sheets file you want to log too. Can be specified using the 'FILE' environment variable.", nargs='*')
-        parser.add_argument("--worksheet", default=os.environ.get('WORKSHEET') , help="Specify which worksheet you want to log too. Can be specified using the 'WORKSHEET' environment variable.", nargs='*')
-        parser.add_argument("--batchsize", default=int(os.environ.get('BATCHSIZE') or '0') , help="Specify the read/write batch size. Not Required. Default is 10. Can be specified using the 'STORELOGS' environment variable.")
+        parser.add_argument("--csv", action="store_true", help="csv")
+        parser.add_argument("--json", default=os.environ.get('JSON'), help="Google API Credentials json file name (file must be in root folder). Can be specified using the 'JSON' environment variable.", nargs='*')
+        parser.add_argument("--file", default=os.environ.get('FILE'), help="Specify which google sheets file you want to log too. Can be specified using the 'FILE' environment variable.", nargs='*')
+        parser.add_argument("--worksheet", default=os.environ.get('WORKSHEET'), help="Specify which worksheet you want to log too. Can be specified using the 'WORKSHEET' environment variable.", nargs='*')
+        parser.add_argument("--batchsize", default=int(os.environ.get('BATCHSIZE') or '0'), help="Specify the read/write batch size. Not Required. Default is 10. Can be specified using the 'STORELOGS' environment variable.")
 
     def load_parse_args(self, args):
         global verbose
@@ -42,12 +45,14 @@ class main(plugin_collection.Plugin):
             args.worksheet = ' '.join(args.worksheet)
 
         if args.mlog:
-            if args.json and args.file and args.worksheet:
-                self.mlog = args.mlog
+            self.mlog = args.mlog
+            self.batchsize = args.batchsize
+            if args.csv:
+                self.csv = args.csv
+            elif args.json and args.file and args.worksheet:
                 self.gauth_json = args.json
                 self.file_name = args.file
                 self.worksheet_name = args.worksheet
-                self.batchsize = args.batchsize
             else:
                 print('Metrics log argument uses google sheets api and requires, Google API Credentials json file name (file must be in root folder), google sheet file name and worksheet name.')
                 print('ex: --mlog  --batchsize [Number (Not Required)] --json [Json File Name] --file [Google Sheet File Name] --worksheet [Worksheet name]')
@@ -98,29 +103,39 @@ class main(plugin_collection.Plugin):
                     break
                 logging_range[0] = txn_seqNo + 1
                 logging_range[1] = txn_seqNo + BATCHSIZE + 1 # put if here to have end of txxn if at end
-                exit()
             print(f'{txn_seqNo}/{txn_range[1]} Transactions logged! {num_of_new_txn} New Transactions. Done!')
 
 
     def find_last(self):
-        authD_client = gspread_authZ(self.gauth_json)
-        sheet = authD_client.open(self.file_name).worksheet(self.worksheet_name)
-        first_row = sheet.row_values(2)
-        if not first_row:
-            sheet.delete_row(2)
-            self.find_last()
-        else:
-            last = int(first_row[0]) # returns as str casting to int
+        if not self.csv:
+            authD_client = gspread_authZ(self.gauth_json)
+            sheet = authD_client.open(self.file_name).worksheet(self.worksheet_name)
+            first_row = sheet.row_values(2)
+            if not first_row:
+                sheet.delete_row(2)
+                self.find_last()
+            else:
+                last = int(first_row[0]) # returns as str casting to int
+        
+        if self.csv:
+            path = os.path.dirname(__file__)
+            with open(path + '/CSVlog.txt') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                line_count = 0
+                for row in csv_reader:
+                    if line_count == 0:
+                        last = int(row[0])
+                        break
         return(last)
 
     def metrics(self, maintxr_response, network_name, txn_range):
-        authD_client = gspread_authZ(self.gauth_json)
-        try:
-            sheet = authD_client.open(self.file_name).worksheet(self.worksheet_name) # Open sheet
-        except:
-            print("\033[1;31;40mUnable to upload data to sheet! Please check file and worksheet name and try again.")
-            print(f'File name entered: {self.file_name}. Worksheet name entered: {self.worksheet_name}.\033[m')
-            exit()
+        # authD_client = gspread_authZ(self.gauth_json)
+        # try:
+        #     sheet = authD_client.open(self.file_name).worksheet(self.worksheet_name) # Open sheet
+        # except:
+        #     print("\033[1;31;40mUnable to upload data to sheet! Please check file and worksheet name and try again.")
+        #     print(f'File name entered: {self.file_name}. Worksheet name entered: {self.worksheet_name}.\033[m')
+        #     exit()
         num_of_txn = 0
 
         for txn in maintxr_response:
@@ -194,12 +209,18 @@ class main(plugin_collection.Plugin):
             
             row = [txn_seqNo, txn_type, txn_time, endorser, txn_from, txn_date, REVOC_REG_ENTRY, REVOC_REG_DEF, CLAIM_DEF, NYM, ATTRIB, SCHEMA]
             print(row)
-            sheet.insert_row(row, 2,value_input_option='USER_ENTERED')
+
+            path = os.path.dirname(__file__)
+            with open(path + '/CSVlog.txt','a') as csv_file:
+                employee_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONE)
+                employee_writer.writerow(row)
+
+            # sheet.insert_row(row, 2,value_input_option='USER_ENTERED')
             num_of_txn += 1
             if txn_seqNo == txn_range[1]:
                 break
             # This is to make sure we don't run into the google api rate limit.
-            time.sleep(2)
+            # time.sleep(2)
 
         print(f'\033[1;92;40m{num_of_txn} transactions added to {self.file_name} in sheet {self.worksheet_name}.\033[m')
         return(txn_seqNo)
